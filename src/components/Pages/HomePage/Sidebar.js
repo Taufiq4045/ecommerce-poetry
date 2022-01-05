dfdcca982e65bd4e8d86d72d7d2c8096e4889c3c
Mohd Taufiq <87647852+Taufiq4045@users.noreply.github.com>
2022-01-05 11:39:22 +0530

    Update Sidebar.js

--- src/components/Pages/HomePage/Sidebar.js
+++ src/components/Pages/HomePage/Sidebar.js
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   sidebarAboutBox: {
@@ -18,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Sidebar(props) {
   const classes = useStyles();
-  const { archives, description, title } = props;
+  const { description, title } = props;
 
   return (
     <Grid item xs={12} md={4}>
@@ -28,19 +27,6 @@ export default function Sidebar(props) {
         </Typography>
         <Typography>{description}</Typography>
       </Paper>
-      <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
-        Archives
-      </Typography>
-      {archives.map((archive) => (
-        <Link
-          display="block"
-          variant="body1"
-          href={archive.url}
-          key={archive.title}
-        >
-          {archive.title}
-        </Link>
-      ))}
     </Grid>
   );
 }
